1424b4444fb883fa1660e0c2add00ca692bc75a1
gogojojo <joseph.gochua@gmail.com>
2015-03-25 02:05:16 -0700

    removed turbo links

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,11 +1,10 @@
 $(function() {
-  // Handler for .ready() called.
 
-  $('a').click(function(){
+  $('#left').click(function(){
       $('html, body').animate({
           scrollLeft: $( $(this).attr('href') ).offset().left
       }, 900);
       return false;
   });
-  
+
 });
