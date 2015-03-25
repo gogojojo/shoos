b3b56e27e689f3822e9fbc8d8b34934a85daa8fa
gogojojo <joseph.gochua@gmail.com>
2015-03-25 16:58:45 -0700

    animation with clicks

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,6 +1,9 @@
 $(function() {
-
+  animateshoe('.jordanI')
   $('.left').click(function(){
+      var clas = $(this).attr('href').replace('#','.')
+      console.log(clas)
+      animateshoe(clas)
       $('html, body').animate({
           scrollLeft: $( $(this).attr('href') ).offset().left
       }, 900);
@@ -8,11 +11,29 @@ $(function() {
   });
 
   $('.right').click(function(){
+      var clas = $(this).attr('href').replace('#','.')
+      console.log(clas)
+      animateshoe(clas)
       $('html, body').animate({
           scrollLeft: $( $(this).attr('href') ).offset().left
       }, 900);
       return false;
   });
+
+  function animateshoe($class){
+  
+  $($class).each(function(i){
+    time=200+(100*i)
+    icon=Math.floor((Math.random()*16));
+    $(this).css({
+      'top':'-500px',
+    }).delay(time).animate({
+      'opacity':'1',
+      'top':'0',
+      'left':'0'
+    },700)
+  })
+  }
 
 
 });
