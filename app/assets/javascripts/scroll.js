a6c53c2a36df69486993e337f12e6e48f0a4e0fa
gogojojo <joseph.gochua@gmail.com>
2015-03-25 16:33:50 -0700

    scroll buttons works

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,10 +1,18 @@
 $(function() {
 
-  $('#left').click(function(){
+  $('.left').click(function(){
       $('html, body').animate({
           scrollLeft: $( $(this).attr('href') ).offset().left
       }, 900);
       return false;
   });
+
+  $('.right').click(function(){
+      $('html, body').animate({
+          scrollLeft: $( $(this).attr('href') ).offset().left
+      }, 900);
+      return false;
+  });
+
 
 });
